c5441b77086c65519c4bdcb3dd9c412262e91107
Francisca <fran.agallardo04@gmail.com>
2020-08-01 00:59:33 -0400

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { PropiedadesComponent } from './pages/propiedades/propiedades.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent,  pathMatch: 'full'},
+  { path: 'contacto', component: ContactoComponent,  pathMatch: 'full'},
+  { path: 'propiedades', component: PropiedadesComponent,  pathMatch: 'full'},
+  { path: '**', component: InicioComponent,  pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { PropiedadesComponent } from './pages/propiedades/propiedades.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ContactoComponent,
+    PropiedadesComponent
   ],
   imports: [
     BrowserModule,
